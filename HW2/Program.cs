5c93b391e1680aee321bb925aa1ae765c9c54b58
genesisvejar <genesisvejar09@gmail.com>
2025-09-08 20:46:44 -0400

    change first switch case and did case 1

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -19,53 +19,59 @@ for (int i = 0; i < maxPets; i++)
     switch (i)
     {
         case 0:
-            ourAnimals[i, 0] = "dog";
-            ourAnimals[i, 1] = "d1";
-            ourAnimals[i, 2] = "2";
-            ourAnimals[i, 3] = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
-            ourAnimals[i, 4] = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
-            ourAnimals[i, 5] = "lola";
+            animalSpecies = "dog";
+            animalID = "d1";
+            animalAge = "2";
+            animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+            animalNickname = "lola";
             break;
-
         case 1:
-            ourAnimals[i, 0] = "dog";
-            ourAnimals[i, 1] = "d2";
-            ourAnimals[i, 2] = "9";
-            ourAnimals[i, 3] = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
-            ourAnimals[i, 4] = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
-            ourAnimals[i, 5] = "loki";
+            animalSpecies = "dog";
+            animalID = "d2";
+            animalAge = "9";
+            animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+            animalNickname = "loki";
             break;
-
         case 2:
-            ourAnimals[i, 0] = "cat";
-            ourAnimals[i, 1] = "c3";
-            ourAnimals[i, 2] = "1";
-            ourAnimals[i, 3] = "small whiteemale weighing about 8 pounds. litter box trained.";
-            ourAnimals[i, 4] = "friendly";
-            ourAnimals[i, 5] = "Puss";
+            animalSpecies = "cat";
+            animalID = "c3";
+            animalAge = "1";
+            animalPhysicalDescription = "small whiteemale weighing about 8 pounds. litter box trained.";
+            animalPersonalityDescription = "friendly";
+            animalNickname = "Puss";
             break;
-
         case 3:
-            ourAnimals[i, 0] = "cat";
-            ourAnimals[i, 1] = "c4";
-            ourAnimals[i, 2] = "?";
-            ourAnimals[i, 3] = "";
-            ourAnimals[i, 4] = "";
-            ourAnimals[i, 5] = "";
+            animalSpecies = "cat";
+            animalID = "c4";
+            animalAge = "?";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            break;
+        default:
+            animalSpecies = "";
+            animalID = "";
+            animalAge = "";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
             break;
 
-        default:
-            ourAnimals[i, 0] = "";
-            ourAnimals[i, 1] = "";
-            ourAnimals[i, 2] = "";
-            ourAnimals[i, 3] = "";
-            ourAnimals[i, 4] = "";
-            ourAnimals[i, 5] = "";
-            break;
 
     }
-
+    
+ourAnimals[i, 0] = "ID #: " + animalID;
+ourAnimals[i, 1] = "Species: " + animalSpecies;
+ourAnimals[i, 2] = "Age: " + animalAge;
+ourAnimals[i, 3] = "Nickname: " + animalNickname;
+ourAnimals[i, 4] = "Physical Description: " + animalPhysicalDescription;
+ourAnimals[i, 5] = "Personality Description: " + animalPersonalityDescription;
 }
+
+
+
 // display the top-level menu options
 
 do
@@ -91,71 +97,81 @@ do
     Console.ReadLine();
 
     switch (menuSelection)
-{   case "1":
-       // List all of our current pet information
-        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
-    case "2":
-        // Add a new animal friend to the ourAnimals array
-        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
-    case "3":
-        // Ensure animal ages and physical descriptions are complete
-        Console.WriteLine("Challenge Project - please check back soon to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
-    case "4":
-        // Ensure animal nicknames and personality descriptions are complete
-        Console.WriteLine("Challenge Project - please check back soon to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
-    case "5":
-        // Edit an animal's age
-        Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
-    case "6":
-        // Edit an animal's personality description
-       Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
-    case "7":
-        // Display all cats with a specified characteristic
-        Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
-    case "8":
-        // Display all dogs with a specified characteristic
-        Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
-    case "exit":
-    case "Exit":
-    case "EXIT":
-        // Exit the program
-         Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
-        break;
-    
-    default:
-    // Invalid menu selection
-        Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
-        Console.WriteLine("Press the Enter key to continue.");
-        readResult = Console.ReadLine();
-        break;
-}
+    {
+        case "1":
+            // List all of our current pet information
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < 6; j++)
+                    {
+                        Console.WriteLine(ourAnimals[i, j]);
+                    }
+                }
+            }
+
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "2":
+            // Add a new animal friend to the ourAnimals array
+            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "3":
+            // Ensure animal ages and physical descriptions are complete
+            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "4":
+            // Ensure animal nicknames and personality descriptions are complete
+            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "5":
+            // Edit an animal's age
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "6":
+            // Edit an animal's personality description
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "7":
+            // Display all cats with a specified characteristic
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "8":
+            // Display all dogs with a specified characteristic
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "exit":
+        case "Exit":
+        case "EXIT":
+            // Exit the program
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            break;
+
+        default:
+            // Invalid menu selection
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+    }
 
 }
 while (menuSelection != "exit" && menuSelection != "Exit" && menuSelection != "EXIT");
-{ 
 
-}
